3f882ec5fdd37e3d7b1f2f7592eb8e6a9bd484e0
Louis Rousseau <louisrousseau@gmail.com>
2015-11-06 14:09:11 -0500

    Removed superfluous include
    Change-Id: I79472d7ae4381fbd7b100549a89416fe64fba191

--- Android.mk
+++ Android.mk
@@ -19,12 +19,11 @@
 # to only building on ARM if they include assembly. Individual makefiles
 # are responsible for having their own logic, for fine-grained control.
 
+LOCAL_PATH := $(call my-dir)
+
 ifeq ($(BOARD_VENDOR),motorola-qcom)
 ifneq ($(filter msm8960 msm8226,$(TARGET_BOARD_PLATFORM)),)
 
-LOCAL_PATH := $(call my-dir)
-
 include $(call all-makefiles-under,$(LOCAL_PATH))
-include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
 endif
